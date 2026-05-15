d7f791fc64e7e050b802e10db10c1931236162c0
RDeveLabs <rdevelopamd@gmail.com>
2026-05-15 20:38:24 +0700

    test fetch data

--- src/main.js
+++ src/main.js
@@ -1,38 +1,38 @@
 import axios from "axios";
 
-// document.getElementById("tombol").addEventListener("click", async () => {
-// await axios
-//   .get("https://api.rdevelabs.biz.id/data", {
-//     headers: {
-//       "x-rdl": "ramdeveloper",
-//     },
-//   })
-//   .then((response) => {
-//     console.log(response.data);
-//   });
-//   .catch((e) => {console.log("error", e)})
-//   .finally(() => {console.log("berhasil");})
-// })
 async function ambilData() {
-  const response = await axios.get("https://api.rdevelabs.biz.id/data", {
-    headers: {
-      "x-rdl": "ramdeveloper",
-    },
-  });
+  try {
+    const response = await axios.get("https://api.rdevelabs.biz.id/data", {
+      headers: {
+        "x-rdl": "ramdeveloper",
+      },
+    });
 
-  const data = response.data.file;
-  console.log(data);
-  data.map((d) => {
-    document.getElementById("tabel").innerHTML = `<tr>`+=
-      `<th>${d.id}</th>` +=
-      `<td>${d.nama}</td>` +=
-      `<td>${d.nim}</td>` +=
-      `<td>${d.kelas}</td>` +=
-      `<td>${d.dari_pertemuan} sampai ${d.sampai_pertemuan}</td>` +=
-      `<td>${d.ukuran}</td>` +=
-      `<td>${d.waktu}</td>` +=
-    `</tr>`;
-  });
+    const data = response.data.file;
+    console.log(data);
+
+    let isiTabel = "";
+
+    data.forEach((d, index) => {
+      isiTabel += `
+        <tr>
+          <th>${index + 1}</th>
+          <td>${d.nama}</td>
+          <td>${d.nim}</td>
+          <td>${d.kelas}</td>
+          <td>${d.dari_pertemuan} sampai ${d.sampai_pertemuan}</td>
+          <td>${d.ukuran} KB</td>
+          <td>${d.waktu}</td>
+        </tr>
+      `;
+    });
+
+    document.getElementById("tabel").innerHTML = isiTabel;
+
+  } catch (error) {
+    console.error("Gagal mengambil data:", error);
+    document.getElementById("tabel").innerHTML = `<tr><td colspan="7" style="text-align:center; color:red;">Gagal memuat data</td></tr>`;
+  }
 }
 
 ambilData();
